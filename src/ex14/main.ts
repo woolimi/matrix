dab6436165ea5a38635a1eda7084d367030b211e
Woolim PARK <wpark@student.42.fr>
2024-07-03 16:49:30 +0200

    ex14 fix

--- src/ex14/main.ts
+++ src/ex14/main.ts
@@ -2,7 +2,7 @@ import { projection } from "../utils/projection";
 import fs from "fs";
 
 const degToRad = (deg: number) => (deg * Math.PI) / 180;
-const m = projection(degToRad(70), 1, 1, 20);
+const m = projection(degToRad(110), 1, 3, 10);
 
 const txt = m.value.map((row) => row.map((el) => el.value).join(", ")).join("\n");
 console.log(txt);
--- src/utils/projection.ts
+++ src/utils/projection.ts
@@ -9,10 +9,19 @@ export const projection = (fov: number, ratio: number, near: number, far: number
   }
 
   const m = Matrix.from(new Array(4).fill(0).map(() => new Array(4).fill(new R(0))));
+  // Column major order
   m.value[0][0] = new R(1 / (ratio * Math.tan(fov / 2)));
   m.value[1][1] = new R(1 / Math.tan(fov / 2));
   m.value[2][2] = new R((far + near) / (near - far));
   m.value[2][3] = new R(-1);
   m.value[3][2] = new R((2 * far * near) / (near - far));
+
+  // Row major order
+  // m.value[0][0] = new R(1 / (ratio * Math.tan(fov / 2)));
+  // m.value[1][1] = new R(1 / Math.tan(fov / 2));
+  // m.value[2][2] = new R((far + near) / (near - far));
+  // m.value[2][3] = new R((2 * far * near) / (near - far));
+  // m.value[3][2] = new R(-1);
+
   return m;
 };
